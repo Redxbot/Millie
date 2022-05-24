03b2526f7bdca110641d57685b8f24ef8e5d4c77
Redxbot <105542141+Redxbot@users.noreply.github.com>
2022-05-24 14:24:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['918113921898'] // Put your number here
+global.owner = ['918129495451'] // Put your number here
 global.mods =  JSON.parse(fs.readFileSync('./src/moderator.json'))// Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium users do not need a limit
 global.tod = JSON.parse(fs.readFileSync('./src/tod.json'))
@@ -33,7 +33,7 @@ global.APIKeys = { // The APIKey is here
 
 // Sticker WM
 
-global.packname = '💋 ᴍɪʟʟɪᴇ 💋'
+global.packname = 'ᎪᏀιɳเន៩Ʀ☻✪'
 
 
 global.wait = '_*Please Wait..*_'
